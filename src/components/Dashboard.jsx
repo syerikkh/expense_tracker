31949e51163f8b09f57250523904209e9bedb8e0
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2024-01-26 19:24:14 +0800

    01.26

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { AppIcon } from './Icons/AppIcon'
+import { GreenDot } from './Icons/GreenDot'
+import { LeadingIcon } from './Icons/LeadingIcon'
+import { BlueDot } from './Icons/BlueDot'
 
 export const Dashboard = () => {
     return (
-        <div className='w-[100vw] h-[100vh] flex flex-col'>
-            <div className='w-full px-[120px] py-6'>
+        <div className='w-[100vw] h-[100vh] flex flex-col bg-[#F3F4F6]'>
+            <div className='w-full px-[340px] py-6 bg-white'>
                 <div className='flex justify-between '>
                     <div className='flex gap-6'>
                         <AppIcon />
@@ -17,13 +20,62 @@ export const Dashboard = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-full px-[120px]'>
-                <div className='flex'>
-                    <div className='w-96 h-72 bg-[#0166FF] rounded-xl p-10'>
-                        <AppIcon />
+            <div className='w-full px-[340px] mt-9'>
+                <div className='flex justify-between'>
+                    <div>
+                        <img src="Large.png" alt="" />
+                    </div>
+                    <div className='w-96  h-[216px] rounded-2xl bg-white'>
+                        <div className='h-14 w-full border-b-2 rounded-t-2xl p-6 flex  items-center gap-3'>
+                            <GreenDot />
+                            <h1 className='font-semibold'>Your Income</h1>
+                        </div>
+                        <div className='p-6 flex flex-col gap-4'>
+                            <div>
+                                <h1 className='font-bold text-2xl'>1,200,000 ₮</h1>
+                                <p className='text-[#64748B]'>Your Income Amount</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <LeadingIcon />
+                                <p>32% from last month</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-96  h-[216px] rounded-2xl bg-white'>
+                        <div className='h-14 w-full border-b-2 rounded-t-2xl p-6 flex  items-center gap-3'>
+                            <BlueDot />
+                            <h1 className='font-semibold'>Total Expenses</h1>
+                        </div>
+                        <div className='p-6 flex flex-col gap-4'>
+                            <div>
+                                <h1 className='font-bold text-2xl'>-1,200,000 ₮</h1>
+                                <p className='text-[#64748B]'>Your Income Amount</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <LeadingIcon />
+                                <p>32% from last month</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div className='flex justify-between mt-9'>
+                    <div className='w-[588px] h-[322px] bg-white rounded-2xl'>
+                        <div className='h-14 w-full p-6 border-b-2 flex items-center'>
+                            <h1 className='font-semibold'>Income - Expense</h1>
+                        </div>
+                        <div className='py-8 px-6'>
+                            <div class="w-full">
+                                <canvas
+                                    data-te-chart="bar"
+                                    data-te-labels="['Monday', 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday ']"
+                                    data-te-dataset-data="[2112, 2343, 2545, 3423, 2365, 1985, 987]">
+                                </canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-[588px] h-36 bg-white rounded-2xl'></div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
